c6578f2e3cfae124b486a072baeb7040883bbb40
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-05-15 14:16:27 +0200

    Adding portfolio deployment

--- terraform-modules/deployments/main.tf
+++ terraform-modules/deployments/main.tf
@@ -31,3 +31,13 @@ module "lego_scraper_deployment" {
   port              = 9000
   server_ip         = var.server_ip
 }
+
+module "portfolio_deployment" {
+  source            = "../reusable-modules/deployment"
+  name              = "portfolio"
+  health_check_path = "/Healthcheck.html"
+  image_tag         = "2"
+  image_url         = "docker.io/tomondre/portfolio"
+  port              = 80
+  server_ip         = var.server_ip
+}
