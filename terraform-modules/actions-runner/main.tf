a3ee8b8dce26457695243d9e139156ef6fc0b2d0
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-11-24 19:38:12 +0100

    Actions secret setup

--- terraform-modules/actions-runner/main.tf
+++ terraform-modules/actions-runner/main.tf
@@ -7,18 +7,72 @@ terraform {
       tags = ["actions-runner"]
     }
   }
-  required_providers {
-    kubectl = {
-      source = "gavinbunney/kubectl"
-    }
-  }
+}
+
+provider "github" {
+  token = var.github_token
 }
 
 provider "kubernetes" {
   config_path = "C:\\Users\\PC1\\.kube\\config"
 }
 
-module "celebrator_3000" {
-  source = "../reusable-modules/runner-deployment"
-  repo_name = "celebrator-3000"
+resource "github_actions_secret" "notes_dockerhub_username" {
+  repository  = "notes-website"
+  secret_name = "DOCKERHUB_USERNAME"
+  plaintext_value = var.dockerhub_username
+}
+
+resource "github_actions_secret" "notes_dockerhub_token" {
+  repository  = "notes-website"
+  secret_name = "DOCKERHUB_TOKEN"
+  plaintext_value = var.dockerhub_token
+}
+
+resource "github_actions_secret" "notes_cloudflare_token" {
+  repository = "notes-website"
+  secret_name = "CLOUDFLARE_API_TOKEN"
+  plaintext_value = var.cloudflare_api_token
+}
+
+resource "github_actions_secret" "notes_kubernetes_config_file" {
+  repository = "notes-website"
+  secret_name = "KUBE_CONFIG"
+  plaintext_value = var.kube_config
+}
+
+resource "github_actions_secret" "notes_terraform_cloud_token" {
+  repository  = "notes-website"
+  secret_name = "TERRAFORM_TOKEN"
+  plaintext_value = var.terraform_token
+}
+
+resource "github_actions_secret" "portfolio_dockerhub_username" {
+  repository  = "portfolio-angular"
+  secret_name = "DOCKERHUB_USERNAME"
+  plaintext_value = var.dockerhub_username
+}
+
+resource "github_actions_secret" "portfolio_dockerhub_token" {
+  repository  = "portfolio-angular"
+  secret_name = "DOCKERHUB_TOKEN"
+  plaintext_value = var.dockerhub_token
+}
+
+resource "github_actions_secret" "portfolio_cloudflare_token" {
+  repository = "portfolio-angular"
+  secret_name = "CLOUDFLARE_API_TOKEN"
+  plaintext_value = var.cloudflare_api_token
+}
+
+resource "github_actions_secret" "portfolio_kubernetes_config_file" {
+  repository = "portfolio-angular"
+  secret_name = "KUBE_CONFIG"
+  plaintext_value = var.kube_config
+}
+
+resource "github_actions_secret" "portfolio_terraform_cloud_token" {
+  repository  = "portfolio-angular"
+  secret_name = "TERRAFORM_TOKEN"
+  plaintext_value = var.terraform_token
 }
---
+++ terraform-modules/actions-runner/variables.tf
@@ -0,0 +1,23 @@
+variable "dockerhub_username" {
+  type = string
+}
+
+variable "dockerhub_token" {
+  type = string
+}
+
+variable "cloudflare_api_token" {
+  type = string
+}
+
+variable "kube_config" {
+  type = string
+}
+
+variable "github_token" {
+  type = string
+}
+
+variable "terraform_token" {
+  type = string
+}
