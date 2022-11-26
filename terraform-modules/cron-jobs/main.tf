50e79c90c666b6c949e68d5d7dc165ad32fa559e
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-11-26 14:35:32 +0100

    .

--- terraform-modules/cron-jobs/main.tf
+++ terraform-modules/cron-jobs/main.tf
@@ -8,38 +8,3 @@ terraform {
   }
 }
 
-provider "kubernetes" {
-  config_path = "C:\\Users\\PC1\\.kube\\config"
-}
-
-resource "kubernetes_cron_job_v1" "celebrations_email" {
-  metadata {
-    name = var.name
-  }
-  spec {
-    schedule           = var.cron
-    concurrency_policy = "Replace"
-    job_template {
-      metadata {}
-      spec {
-        ttl_seconds_after_finished = 10
-        template {
-          metadata {}
-          spec {
-            container {
-              name  = var.name
-              image = "${var.image}:${var.tag}"
-              dynamic "env" {
-                for_each = var.env
-                content {
-                  name  = env.key
-                  value = env.value
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
--- terraform-modules/cron-jobs/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-cron  = "0 6 * * *"
-image = "docker.io/tomondre/celebrator-3000"
-name  = "celebrator-3000"
-tag   = 4
