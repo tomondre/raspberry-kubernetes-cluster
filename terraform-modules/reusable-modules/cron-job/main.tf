940f9645f223f8abf802aee8e9637b04e1ae2190
tomas.ondrejka2000@gmail.com <304150@via.dk>
2022-11-26 21:54:29 +0100

    Removing backend from module

--- terraform-modules/reusable-modules/cron-job/main.tf
+++ terraform-modules/reusable-modules/cron-job/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  cloud {
-    organization = "raspberry-kubernetes-cluster"
-
-    workspaces {
-      tags = ["cron-jobs"]
-    }
-  }
-}
-
-provider "kubernetes" {
-  config_path = "C:\\Users\\PC1\\.kube\\config"
-}
-
 resource "kubernetes_cron_job" "celebrations_email" {
   metadata {
     name = var.name
