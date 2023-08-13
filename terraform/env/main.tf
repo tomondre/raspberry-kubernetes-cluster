a964e264350cb5b6c6a82c97554cb7a8ca5c8061
tomondre <tomas.ondrejka2000@gmail.com>
2023-08-13 19:13:20 +0200

    Adding tfvars

--- terraform/env/main.tf
+++ terraform/env/main.tf
@@ -10,7 +10,38 @@ terraform {
 
 locals {
   host = "cloud.tomondre.com"
-  service_name = "nextcloud"
+  service_name = nextcloud
+}
+
+resource "helm_release" "cloud_helm_release" {
+  chart = "nextcloud"
+  name  = "nextcloud"
+  repository = "https://nextcloud.github.io/helm/"
+
+  set {
+    name  = "nextcloud.host"
+    value = "https://${local.host}"
+  }
+
+  set {
+    name  = "nextcloud.configs.s3.config.php"
+    value = <<EOF
+  <?php
+  $CONFIG = array (
+    'objectstore' => array(
+    'class' => '\\OC\\Files\\ObjectStore\\S3',
+    'arguments' => array(
+    'bucket'     => 'my-bucket',
+    'autocreate' => true,
+    'key'        => '${var.nextcloud_aws_access_key}',
+    'secret'     => '${var.nextcloud_aws_secret_key}',
+    'region'     => '${var.nextcloud_aws_region}',
+    'use_ssl'    => true
+    )
+  )
+  );
+EOF
+  }
 }
 
 #resource "kubernetes_manifest" "traefik-ingress-route" {
---
+++ terraform/env/variables.tf
@@ -0,0 +1,11 @@
+variable "nextcloud_aws_access_key" {
+  type = string
+}
+
+variable "nextcloud_aws_secret_key" {
+  type = string
+}
+
+variable "nextcloud_aws_region" {
+  type = string
+}
